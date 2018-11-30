d963d1bf34c208ed752a079b67c413ce2bbc0e7b
AllenTroyer <allentroyer@gmail.com>
2018-11-30 17:50:28 -0500

    Radio Button Challenge

---
+++ RadioButtonChallenge/RadioButtonChallenge/Default.aspx.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace RadioButtonChallenge
+{
+    public partial class Default : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void OKButton_Click(object sender, EventArgs e)
+        {
+            if (PencilRadioButton.Checked)
+            {
+                ResultLabel.Text = "This is a sample Achievement Test Profile";
+             //   ResultImage.ImageUrl = "magnifying glass.png";
+                ResultImage.ImageUrl = "Sample Profile 2016png_Page1.png";
+            }
+            else if (PenRadioButton.Checked)
+            {
+                ResultLabel.Text = "This is a sample Achievement Test Analysis";
+                ResultImage.ImageUrl = "Sample Analysis 2016png_Page1.png";
+            }
+            else if (PhoneRadioButton.Checked)
+            {
+                ResultLabel.Text = "This is a sample Achievement Test Stanine Summary";
+                ResultImage.ImageUrl = "Sample Stanine 2016png_Page1.png";
+            }
+            else 
+            {
+                ResultLabel.Text = "This is a sample Achievement Test Percentile Summary";
+                ResultImage.ImageUrl = "Sample Percentile ADSSpng_Page1.png";
+            }
+        }
+    }
+}
---
+++ RadioButtonChallenge/RadioButtonChallenge/obj/Release/Package/PackageTmp/Default.aspx
@@ -0,0 +1,33 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Default.aspx.cs" Inherits="RadioButtonChallenge.Default" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            Select the sample you want:<br />
+            <br />
+            <asp:RadioButton ID="PencilRadioButton" runat="server" Checked="True" GroupName="noteGroup" Text="Individual Profile" />
+            <br />
+            <asp:RadioButton ID="PenRadioButton" runat="server" GroupName="noteGroup" Text="Individual Analysis" />
+            <br />
+            <asp:RadioButton ID="PhoneRadioButton" runat="server" GroupName="noteGroup" Text="Class Stanines" />
+            <br />
+            <asp:RadioButton ID="TabletRadioButton" runat="server" GroupName="noteGroup" Text="Class Percentiles" />
+            <br />
+            <br />
+            <asp:Button ID="OKButton" runat="server" OnClick="OKButton_Click" Text="OK" />
+            <br />
+            <br />
+            <asp:Label ID="ResultLabel" runat="server"></asp:Label>
+            <br />
+            <br />
+            <asp:Image ID="ResultImage" runat="server" />
+        </div>
+    </form>
+</body>
+</html>
